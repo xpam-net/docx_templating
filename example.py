473ac9cd53133e215fee1f7b0adeb6aabb193757
Andrey Luzhin <admin@xpam.net>
2020-04-06 12:59:53 +0200

    Reformat code

--- example.py
+++ example.py
@@ -2,11 +2,12 @@
 from docxt import DocxT
 
 substitutes = {
-    'hello' : 'Hello! Привет! ¡Hola!',
-    'test'  : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    'bye'   : 'Bye! Пока! ¡Adiós!',
-    'chars' : '[ < > ? . * { } & % " \' ]',
-    'blank' : '' # used to demonstate how to preserve some text inside double curly braces. Just insert {{blank}} somewhere inside such text
+    'hello': 'Hello! Привет! ¡Hola!',
+    'test': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'bye': 'Bye! Пока! ¡Adiós!',
+    'chars': '[ < > ? . * { } & % " \' ]',
+    'blank': ''
+    # used to demonstate how to preserve some text inside double curly braces. Just insert {{blank}} somewhere inside such text
 }
 
 # Opening template file
